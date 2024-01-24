5e6fb2b88b2513ecc5547c6a9a49329c709cfa2e
Sedef Cakmak <cakmaksedef@gmail.com>
2024-01-24 16:03:12 +0300

    added cards

--- src/components/CardBack.js
+++ src/components/CardBack.js
@@ -3,11 +3,12 @@ import cardBack from "../images/bg-card-back.png";
 export default function CardBack() {
   return (
     <div>
-      <div className="logo">
+      <div className="card-back-wrapper">
         <img
           src={cardBack}
           alt={"Card back"}
         />
+        <h1 className="cvc-back">000</h1>
       </div>
     </div>
   );
--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -1,13 +1,19 @@
-import cardFront from "../images/bg-card-front.png";
 import "./CardFront.css";
+import cardFront from "../images/bg-card-front.png";
+
 export default function CardFront() {
   return (
     <div>
-      <div className="logo">
+      <div className="card-front-wrapper">
         <img
           src={cardFront}
           alt={"Card front"}
         />
+        <h1 className="card-number">0000 0000 0000 0000</h1>
+        <div className="wrapper">
+          <h2 className="card-name">Jane Appleseed</h2>
+          <span className="valid-date">00/00</span>
+        </div>
       </div>
     </div>
   );
